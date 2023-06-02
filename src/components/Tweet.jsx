b1d78b3e96634c8e7f285547ff1c982af7ef1103
varelacp <124377343+varelacp@users.noreply.github.com>
2023-06-02 20:09:53 +0100

    done

---
+++ src/components/Tweet.jsx
@@ -0,0 +1,27 @@
+import ProfileImage from './ProfileImage';
+import User from './User';
+import Timestamp from './Timestamp';
+import Message from './Message';
+import Actions from './Actions';
+
+const Tweet = ({ tweet }) => {
+  return (
+    <div className="tweet">
+      <ProfileImage image={tweet.user.image} />
+
+      <div className="body">
+        <div className="top">
+          <User userData={tweet.user} />
+          <Timestamp time={tweet.timestamp} />
+        </div>
+
+        <Message message={tweet.message} />
+        <Actions />
+      </div>
+
+      <i className="fas fa-ellipsis-h"></i>
+    </div>
+  );
+};
+
+export default Tweet;
